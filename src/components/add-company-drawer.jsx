a8455b5013e7bb1ee546f1343a7341738780469c
Nisarga <nisargaargekar2002@gmail.com>
2025-05-24 11:49:50 +0530

    update code

--- src/components/add-company-drawer.jsx
+++ src/components/add-company-drawer.jsx
@@ -1,91 +1,114 @@
-import React from 'react'
-import { useForm } from 'react-hook-form';
-import {z} from 'zod';
-import {zodResolver} from '@hookform/resolvers/zod';
-import { Drawer, DrawerTrigger, DrawerContent, DrawerHeader, DrawerTitle, 
-    DrawerDescription, DrawerFooter, Button } from '@/components/ui/drawer';
-import { Input } from './ui/input';
-import useFetch from '@/hooks/use-fetch';
-import { addNewCompany } from '@/api/apiCompanies';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import useFetch from "@/hooks/use-fetch";
+import { addNewCompany } from "@/api/apiCompanies";
+import { BarLoader } from "react-spinners";
+import { useEffect } from "react";
 
-const shema = z.object({
-    name: z.string().min(1, {message:"Company name is Required"}),
-     resume: z
+const schema = z.object({
+  name: z.string().min(1, { message: "Company name is required" }),
+  logo: z
     .any()
     .refine(
       (file) =>
-        file?.[0] &&
-        (file[0].type === "image/png" ||
-          file[0].type === "image/jpeg"),
-      { message: "Only Images are allowed" }
+        file[0] &&
+        (file[0].type === "image/png" || file[0].type === "image/jpeg"),
+      {
+        message: "Only Images are allowed",
+      }
     ),
-})
+});
 
+const AddCompanyDrawer = ({ fetchCompanies }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
 
-const AddCompanyDrawer = (fnCompanies) => {
+  const {
+    loading: loadingAddCompany,
+    error: errorAddCompany,
+    data: dataAddCompany,
+    fn: fnAddCompany,
+  } = useFetch(addNewCompany);
 
+  const onSubmit = async (data) => {
+    fnAddCompany({
+      ...data,
+      logo: data.logo[0],
+    });
+  };
 
-    const {
-        register,
-        handleSubmit,
-        formState:{errors},
-    }=useForm({
-            resolver:zodResolver(shema),
-        });
-
-
-        const {
-          loading: loadingAddCompany,
-          error:errorAddCompany,
-          data: dataAddCompany,
-          fn:fnAddCompany,
-        } = useFetch(addNewCompany);
-
-      const onsubmit = () => {};
+  useEffect(() => {
+    if (dataAddCompany?.length > 0) {
+      fetchCompanies();
+    }
+  }, [loadingAddCompany]);
 
   return (
     <Drawer>
-  <DrawerTrigger>
-    <Button type='button' size='sm' variant='green'>
-      Add Company
-    </Button>
-  </DrawerTrigger>
-  <DrawerContent>
-  
-
-      <form className='flec gap-2 p-4 pb-0'>
-        <Input palceholder="Company name"{...register("name")}/>
-        <Input 
-        type="file"
-        accept="image/"
-        className="file:text-gray-500" 
-        {...register("logo")}
-        />
-        <Button 
-        type="button"
-        onClick={handleSubmit(onsubmit)}
-        variant="destructive"
-        className="w-40"
-        >
-          Add
+      <DrawerTrigger>
+        <Button type="button" size="sm" variant="secondary">
+          Add Company
         </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Add a New Company</DrawerTitle>
+        </DrawerHeader>
+        <form className="flex gap-2 p-4 pb-0">
+         
+          <Input placeholder="Company name" {...register("name")} />
 
-      </form>
-      {errors.name && <p className='text-res-500'>{errors.name.message}</p>}
-      {errors.logo && <p className='text-res-500'>{errors.logo.message}</p>}
-   
-    
-    
-    <DrawerFooter>
-    
-      <DrawerClose asChild>
-        <Button variant="secondary" type="button">Cancel</Button>
-      </DrawerClose>
-    </DrawerFooter>
-  </DrawerContent>
-</Drawer>
+        
+          <Input
+            type="file"
+            accept="image/*"
+            className=" file:text-gray-500"
+            {...register("logo")}
+          />
 
-  )
-}
+       
+          <Button
+            type="button"
+            onClick={handleSubmit(onSubmit)}
+            variant="destructive"
+            className="w-40"
+          >
+            Add
+          </Button>
+        </form>
+        <DrawerFooter>
+          {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+          {errors.logo && <p className="text-red-500">{errors.logo.message}</p>}
+          {errorAddCompany?.message && (
+            <p className="text-red-500">{errorAddCompany?.message}</p>
+          )}
+          {loadingAddCompany && <BarLoader width={"100%"} color="#36d7b7" />}
+          <DrawerClose asChild>
+            <Button type="button" variant="secondary">
+              Cancel
+            </Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+};
 
 export default AddCompanyDrawer;
