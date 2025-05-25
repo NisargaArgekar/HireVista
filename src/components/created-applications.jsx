09efee8bdab157e4d50a520cd2bd96faae015bfb
Nisarga <nisargaargekar2002@gmail.com>
2025-05-25 19:16:45 +0530

    complete!

--- src/components/created-applications.jsx
+++ src/components/created-applications.jsx
@@ -8,7 +8,7 @@ import ApplicationCard from './application-card';
 
 const CreatedApplications = () => {
 
-      const {user} = useUser();
+    const {user} = useUser();
 
     const { 
     loading: loadingApplications,
@@ -30,7 +30,7 @@ const CreatedApplications = () => {
 return (
     <div className='flex flex-col gap-2'>
      {
-          applications.map((application)=>{
+          (applications || []).map((application)=>{
             return (
               <ApplicationCard key={application.id} application={application}
               isCandidate = {true}
--- src/components/job-card.jsx
+++ src/components/job-card.jsx
@@ -9,8 +9,9 @@ import {
 import {  Heart, MapPinIcon, Trash2 } from 'lucide-react';
 import { Button } from './ui/button';
 import { Link } from 'react-router-dom';
-import { saveJob } from '@/api/apiJobs';
+import { deleteJob, saveJob } from '@/api/apiJobs';
 import useFetch  from '@/hooks/use-fetch'
+import { BarLoader } from 'react-spinners';
 
 const JobCard= ({
   job,
@@ -39,6 +40,16 @@ const JobCard= ({
     onJobSaved();
    };
 
+   const { loading: loadingDeleteJob, fn:fnDeleteJob } = useFetch( deleteJob , {
+    job_id: job.id,
+  });
+
+
+  const handleDeleteJob = async () => {
+    await fnDeleteJob();
+    onJobSaved();
+  }
+
   useEffect(()=>{
     if(savedJob!==undefined)setSaved(savedJob?.length>0);
 
@@ -46,14 +57,20 @@ const JobCard= ({
 
 
 
-  return (<Card  className="flex flex-col">
+  return (
+  <Card  className="flex flex-col">
+    {loadingDeleteJob && (
+      <BarLoader className='mt-4' width={"100%"} color='#36d7b7'/>
+    )}
     <CardHeader>
       <CardTitle className='flex justify-between font-bold'>{job.title}
 
       {isMyJob &&
        (
        <Trash2 fill='red' size={18}
-        className='text-red-300 cursor-pointer'/>
+        className='text-red-300 cursor-pointer'
+        onClick={handleDeleteJob}
+        />
 
        )}
       </CardTitle>
