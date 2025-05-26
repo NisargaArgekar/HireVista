4727cbd9386f4c33bd90084688571002d07d7363
Nisarga <nisargaargekar2002@gmail.com>
2025-05-26 18:32:26 +0530

    progressing

--- src/components/protected-route.jsx
+++ src/components/protected-route.jsx
@@ -1,23 +1,21 @@
-import { useUser } from '@clerk/clerk-react';
-import { Navigate, useLocation } from 'react-router-dom';
+
+import { Navigate, useLocation } from "react-router-dom";
+import { useUser } from "@clerk/clerk-react";
 
 const ProtectedRoute = ({ children }) => {
-  const { isSignedIn, user, isLoaded } = useUser();
+  const { isSignedIn, isLoaded, user } = useUser();
   const { pathname } = useLocation();
 
-  if (!isLoaded) {
-    return <div>Loading...</div>;
+  if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
+    return <Navigate to="/?sign-in=true" />;
   }
 
- 
-  if (!isSignedIn) {
-    return <Navigate to='/?sign-in=true' />;
-  }
-
- 
-  if (!user?.unsafeMetadata?.role && pathname !== '/onboarding') {
-    return <Navigate to='/onboarding' />;
-  }
+  if (
+    user !== undefined &&
+    !user?.unsafeMetadata?.role &&
+    pathname !== "/onboarding"
+  )
+    return <Navigate to="/onboarding" />;
 
   return children;
 };
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,112 +1,105 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
-import { Link } from 'react-router-dom'
-import  { Carousel,CarouselContent,CarouselItem} from '@/components/ui/carousel'
-import companies from '../db/companies.json'
-import que from '../db/que.json'
-import Autoplay from 'embla-carousel-autoplay'
-import {Card,CardContent,CardHeader,CardTitle} from "@/components/ui/card"
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import companies from "../db/companies.json";
+import que from "../db/que.json";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion"
-
-
-
+} from "@/components/ui/accordion";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
-    <main className='flex flex-col gap-10 sm:gap-20 py-10 sm:py-20'>
-      <section className='text-center' >
-        <h1 className='flex flex-col items-center justify-center
-         gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl
-        tracking-tighter py-4' > 
-          Find Your Dream Job  {" "}
-        <span className=' flex items-center gap-2 sm:gap-6'>
-           in <img src='/icon.png' alt='logo'
-        className='h-14 sm:h-24 lg:h-32'/></span>
+    <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
+      <section className="text-center ">
+        <h1 className="flex flex-col items-center justify-center gradient-title font-extrabold text-4xl sm:text-6xl lg:text-8xl tracking-tighter py-4">
+          Find Your Dream Job
+          <span className="flex items-center gap-2 sm:gap-6">
+            and get
+            <img
+              src="/icon.png"
+              className="h-14 sm:h-24 lg:h-32"
+              alt="HireVista Logo"
+            />
+          </span>
         </h1>
-         <p className="text-gray-300 sm:mt-4 text-xs sm:text-xl">
+        <p className="text-gray-300 sm:mt-4 text-xs sm:text-xl">
           Explore thousands of job listings or find the perfect candidate
         </p>
       </section>
-      <div className='flex gap-6 justify-center'>
-        <Link to="/joblisting"> 
-        <Button variant={"green"} size={"xl"}>
-          Find Jobs
+      <div className="flex gap-6 justify-center">
+        <Link to={"/jobs"}>
+          <Button variant="green" size="xl">
+            Find Jobs
           </Button>
         </Link>
-        <Link to="/post-job"> 
-        <Button variant={"destructive"} size={"xl"}>Post a Job</Button>
+        <Link to={"/post-job"}>
+          <Button variant="destructive" size="xl">
+            Post a Job
+          </Button>
         </Link>
       </div>
+      <Carousel
+        plugins={[
+          Autoplay({
+            delay: 2000,
+          }),
+        ]}
+        className="w-full py-10"
+      >
+        <CarouselContent className="flex gap-5 sm:gap-20 items-center">
+          {companies.map(({ name, id, path }) => (
+            <CarouselItem key={id} className="basis-1/3 lg:basis-1/6 ">
+              <img
+                src={path}
+                alt={name}
+                className="h-9 sm:h-14 object-contain"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
 
+      <img src="/banner.jpeg" className="w-full" />
 
-      {/*carousel*/}
-      <Carousel  plugins={[
-        Autoplay({
-          delay: 2000,
-        }),
-      ]}className="w-full py-10">
-      <CarouselContent className='flex gap-5 sm:gap-20 items-center'>
-       {companies.map(({name,id,path})=>{
-        return <CarouselItem key={id} className='basis-1/3 lg:basis-1/6'>
-          <img src={path} alt={name}
-          className='h-9 sm:h-14 object-contain'/>
-        </CarouselItem>
-       })}
-      </CarouselContent>
-    </Carousel>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <Card>
+          <CardHeader>
+            <CardTitle className="font-bold">Job Hunters</CardTitle>
+          </CardHeader>
+          <CardContent>
+           Track your applications and manage everything in one place.
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle className="font-bold">Recruiter</CardTitle>
+          </CardHeader>
+          <CardContent>
+            Companies can post jobs and manage applications easily.
+          Find the right candidates faster with smart tools.
+          </CardContent>
+        </Card>
+      </section>
 
-
-      {/*banner*/}
-      <img src='/banner.jpeg' className='w-full'/>
-
-    <section className='grid grid-cols-1 md:grid-cols-2 gap-4' >
-      {/* cards */}
-      <Card>
-       <CardHeader>
-         <CardTitle>Job hunters</CardTitle>
-         
-        </CardHeader>
-       <CardContent>
-          <p>Track your applications and manage everything in one place.</p>
-       </CardContent>
-      </Card>
-       <Card>
-       <CardHeader>
-         <CardTitle>Recruiter</CardTitle>
-        
-        </CardHeader>
-       <CardContent>
-          <p>Companies can post jobs and manage applications easily.
-          Find the right candidates faster with smart tools.</p>
-       </CardContent>
-      </Card>
-    </section>
-
-
-
-      {/* Accordion */}
-  <Accordion type="single" collapsible>
-    {que.map((que,index)=>{
-     return ( <AccordionItem  key={index} value={`item-${index+1}`}>
-    <AccordionTrigger>{que.question}</AccordionTrigger>
-    <AccordionContent>
-      {que.answer}
-    </AccordionContent>
-  </AccordionItem>
-  );
-
-    })}
-  
-</Accordion>
-
-      
+      <Accordion type="multiple" className="w-full">
+        {que.map((que, index) => (
+          <AccordionItem key={index} value={`item-${index + 1}`}>
+            <AccordionTrigger>{que.question}</AccordionTrigger>
+            <AccordionContent>{que.answer}</AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </main>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
